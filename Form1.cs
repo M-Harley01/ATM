762596e6f70f468f3667badadeba6f1226506f3d
lowballed <114414426+arvin-shafiei@users.noreply.github.com>
2024-03-19 14:37:48 +0000

    some gui stuff

--- Form1.cs
+++ Form1.cs
@@ -6,244 +6,277 @@ namespace assignment3
 {
     public partial class Form1 : Form
     {
+        private BankSystem currentBankSystem;
+        private Account currentAccount;
+        private bool useUnlockedMethods = false;
+    
+        private TextBox registerTxtAccountNumber;
+        private TextBox registerTxtPin;
+        private TextBox txtAccountNumber;
+        private TextBox txtPin;
+        private TextBox txtBalance;
+        private TextBox txtWithdrawAmount;
+        private Button btnAddAccount;
+        private Button btnWithdraw;
+        private Button btnLogin;
+        private Button btnToggleMethod;
+        private Label lblAccountInfo;
+        private Label lblCurrentAccount;
+
         public Form1()
         {
             InitializeComponent();
-
-            Account[] ac = new Account[3];
-            ATM atm;
-
-            ac[0] = new Account(300, 1111, 111111);
-            ac[1] = new Account(750, 2222, 222222);
-            ac[2] = new Account(3000, 3333, 333333);
-
-            atm = new ATM(ac);
+            currentBankSystem = new BankSystem();
+            InitializeFormComponents();
         }
-
+        
         private void Form1_Load(object sender, EventArgs e)
         {
            
         }
-    }
 
-    class Account
-    {
-        private int balance;
-        private int pin;
-        private int accountNum;
-
-        public Account(int balance, int pin, int accountNum)
+        private void InitializeFormComponents()
         {
-            this.balance = balance;
-            this.pin = pin;
-            this.accountNum = accountNum;
-        }
-
-        public int getBalance()
-        {
-            return balance;
-        }
-
-        public void setBalance(int newBalance)
-        {
-            this.balance = newBalance;
-        }
-
-        public Boolean decrementBalance(int amount)
-        {
-            if (this.balance > amount)
+            // ATM Heading
+            Label lblHeading = new Label
             {
-                balance -= amount;
-                return true;
-            }
-            else
+                Text = "ATM",
+                Location = new System.Drawing.Point(20, 10),
+                Width = 300,
+                Height = 50,
+                Font = new System.Drawing.Font("Arial", 24, System.Drawing.FontStyle.Bold)
+            };
+            Controls.Add(lblHeading);
+
+            // Account Number
+            txtAccountNumber = new TextBox
             {
-                return false;
-            }
+                Location = new System.Drawing.Point(20, 70),
+                Width = 200,
+                Tag = "Account Number"
+            };
+            SetPlaceholderText(txtAccountNumber, "Account Number");
+            Controls.Add(txtAccountNumber);
+
+            // PIN
+            txtPin = new TextBox
+            {
+                Location = new System.Drawing.Point(20, 110),
+                Width = 200,
+                Tag = "PIN",
+          
+            };
+            SetPlaceholderText(txtPin, "PIN");
+            Controls.Add(txtPin);
+            
+            // Account Number
+            registerTxtAccountNumber = new TextBox
+            {
+                Location = new System.Drawing.Point(20, 70),
+                Width = 200,
+                Tag = "Account Number"
+            };
+            SetPlaceholderText(txtAccountNumber, "Account Number");
+            Controls.Add(txtAccountNumber);
+
+            // PIN
+            registerTxtPin = new TextBox
+            {
+                Location = new System.Drawing.Point(100, 110),
+                Width = 200,
+                Tag = "PIN",
+          
+            };
+            SetPlaceholderText(txtPin, "PIN");
+            Controls.Add(txtPin);
+
+            // Balance - Used only for adding account
+            txtBalance = new TextBox
+            {
+                Location = new System.Drawing.Point(20, 150),
+                Width = 200,
+                Tag = "Initial Balance"
+            };
+            SetPlaceholderText(txtBalance, "Initial Balance");
+            Controls.Add(txtBalance);
+
+            // Withdraw Amount
+            txtWithdrawAmount = new TextBox
+            {
+                Location = new System.Drawing.Point(20, 190),
+                Width = 200,
+                Tag = "Withdraw Amount"
+            };
+            SetPlaceholderText(txtWithdrawAmount, "Withdraw Amount");
+            Controls.Add(txtWithdrawAmount);
+
+            // Add Account Button
+            btnAddAccount = new Button
+            {
+                Text = "Add Account",
+                Location = new System.Drawing.Point(230, 150)
+            };
+            btnAddAccount.Click += BtnAddAccount_Click;
+            Controls.Add(btnAddAccount);
+
+            // Withdraw Button
+            btnWithdraw = new Button
+            {
+                Text = "Withdraw",
+                Location = new System.Drawing.Point(230, 190)
+            };
+            btnWithdraw.Click += BtnWithdraw_Click;
+            Controls.Add(btnWithdraw);
+
+            // Login Button
+            btnLogin = new Button
+            {
+                Text = "Login",
+                Location = new System.Drawing.Point(230, 110)
+            };
+            btnLogin.Click += BtnLogin_Click;
+            Controls.Add(btnLogin);
+
+            // Toggle Method Button
+            btnToggleMethod = new Button
+            {
+                Text = "Toggle Lock Method",
+                Location = new System.Drawing.Point(20, 230)
+            };
+            btnToggleMethod.Click += BtnToggleMethod_Click;
+            Controls.Add(btnToggleMethod);
+
+            // Account Info Label
+            lblAccountInfo = new Label
+            {
+                Location = new System.Drawing.Point(20, 270),
+                Width = 400,
+                Height = 40
+            };
+            Controls.Add(lblAccountInfo);
+
+            // Current Account Label
+            lblCurrentAccount = new Label
+            {
+                Location = new System.Drawing.Point(20, 310),
+                Width = 400,
+                Height = 40
+            };
+            Controls.Add(lblCurrentAccount);
         }
 
-        public Boolean checkPin(int pinEntered)
+        private void SetPlaceholderText(TextBox textBox, string placeholderText)
         {
-            if (pinEntered == pin)
+            textBox.ForeColor = System.Drawing.Color.Gray;
+            textBox.Text = placeholderText;
+            textBox.Enter += (sender, e) =>
             {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public int getAccountNum()
-        {
-            return accountNum;
-        }
-    }
-
-    class ATM
-    {
-        private Account[] ac;
-        private Account activeAccount = null;
-
-        public ATM(Account[] ac)
-        {
-            this.ac = ac;
-            Console.WriteLine("hello from ATM");
-
-            // an infanite loop to keep the flow of controll going on and on
-            while (true)
-            {
-
-                //ask for account number and store result in acctiveAccount (null if no match found)
-                activeAccount = this.findAccount();
-
-                if (activeAccount != null)
+                if (textBox.Text == placeholderText)
                 {
-                    //if the account is found check the pin 
-                    if (activeAccount.checkPin(this.promptForPin()))
-                    {
-                        //if the pin is a match give the options to do stuff to the account (take money out, view balance, exit)
-                        dispOptions();
-                    }
+                    textBox.Text = "";
+                    textBox.ForeColor = System.Drawing.Color.Black;
+                }
+            };
+            textBox.Leave += (sender, e) =>
+            {
+                if (textBox.Text == "")
+                {
+                    textBox.Text = placeholderText;
+                    textBox.ForeColor = System.Drawing.Color.Gray;
+                }
+            };
+        }
+
+        private void BtnAddAccount_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int accountNumber = int.Parse(txtAccountNumber.Text);
+                int pin = int.Parse(txtPin.Text);
+                int balance = int.Parse(txtBalance.Text);
+                currentBankSystem.AddAccount(accountNumber, pin, balance);
+                MessageBox.Show("Account added successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error adding account: {ex.Message}");
+            }
+        }
+
+        private void BtnWithdraw_Click(object sender, EventArgs e)
+        {
+            if (currentAccount == null)
+            {
+                MessageBox.Show("Please login first.");
+                return;
+            }
+
+            try
+            {
+                int amount = int.Parse(txtWithdrawAmount.Text);
+                var atm = currentBankSystem.DispatchATM();
+                if (useUnlockedMethods)
+                {
+                    Task.Run(() => atm.WithdrawUnlocked(currentAccount, amount)).ContinueWith(t => UpdateAccountInfo(), TaskScheduler.FromCurrentSynchronizationContext());
                 }
                 else
-                {   //if the account number entered is not found let the user know!
-                    Console.WriteLine("no matching account found.");
-                }
-
-                //wipes all text from the console
-                Console.Clear();
-            }
-
-
-        }
-
-        private Account findAccount()
-        {
-            Console.WriteLine("enter your account number..");
-
-            int input = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 0; i < this.ac.Length; i++)
-            {
-                if (ac[i].getAccountNum() == input)
                 {
-                    return ac[i];
+                    Task.Run(() => atm.Withdraw(currentAccount, amount)).ContinueWith(t => UpdateAccountInfo(), TaskScheduler.FromCurrentSynchronizationContext());
                 }
+                MessageBox.Show($"Withdrawal of {amount} initiated.");
             }
-
-            return null;
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error during withdrawal: {ex.Message}");
+            }
         }
 
-        private int promptForPin()
+        private void BtnLogin_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("enter pin:");
-            String str = Console.ReadLine();
-            int pinNumEntered = Convert.ToInt32(str);
-            return pinNumEntered;
-        }
-
-        private void dispOptions()
-        {
-            Console.WriteLine("1> take out cash");
-            Console.WriteLine("2> balance");
-            Console.WriteLine("3> exit");
-
-            int input = Convert.ToInt32(Console.ReadLine());
-
-            if (input == 1)
+            try
             {
-                dispWithdraw();
-            }
-            else if (input == 2)
-            {
-                dispBalance();
-            }
-            else if (input == 3)
-            {
-
-
-            }
-            else
-            {
-
-            }
-
-        }
-
-        private void dispWithdraw()
-        {
-            Console.WriteLine("1> 10");
-            Console.WriteLine("2> 50");
-            Console.WriteLine("3> 500");
-
-            int input = Convert.ToInt32(Console.ReadLine());
-
-            if (input > 0 && input < 4)
-            {
-
-                //opiton one is entered by the user
-                if (input == 1)
+                int accountNumber = int.Parse(txtAccountNumber.Text);
+                int pin = int.Parse(txtPin.Text);
+                var account = currentBankSystem.GetAccount(accountNumber);
+                if (account.CheckPin(pin))
                 {
-
-                    //attempt to decrement account by 10 punds
-                    if (activeAccount.decrementBalance(10))
-                    {
-                        //if this is possible display new balance and await key press
-                        Console.WriteLine("new balance " + activeAccount.getBalance());
-                        Console.WriteLine(" (prese enter to continue)");
-                        Console.ReadLine();
-                    }
-                    else
-                    {
-                        //if this is not possible inform user and await key press
-                        Console.WriteLine("insufficent funds");
-                        Console.WriteLine(" (prese enter to continue)");
-                        Console.ReadLine();
-                    }
+                    currentAccount = account;
+                    lblCurrentAccount.Text = $"Logged into account: {accountNumber}";
+                    UpdateAccountInfo();
                 }
-                else if (input == 2)
+                else
                 {
-                    if (activeAccount.decrementBalance(50))
-                    {
-                        Console.WriteLine("new balance " + activeAccount.getBalance());
-                        Console.WriteLine(" (prese enter to continue)");
-                        Console.ReadLine();
-                    }
-                    else
-                    {
-                        Console.WriteLine("insufficent funds");
-                        Console.WriteLine(" (prese enter to continue)");
-                        Console.ReadLine();
-                    }
+                    MessageBox.Show("Invalid PIN");
                 }
-                else if (input == 3)
-                {
-                    if (activeAccount.decrementBalance(500))
-                    {
-                        Console.WriteLine("new balance " + activeAccount.getBalance());
-                        Console.WriteLine(" (prese enter to continue)");
-                        Console.ReadLine();
-                    }
-                    else
-                    {
-                        Console.WriteLine("insufficent funds");
-                        Console.WriteLine(" (prese enter to continue)");
-                        Console.ReadLine();
-                    }
-                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Login failed: {ex.Message}");
             }
         }
 
-        private void dispBalance()
+        private void BtnToggleMethod_Click(object sender, EventArgs e)
         {
-            if (this.activeAccount != null)
+            useUnlockedMethods = !useUnlockedMethods;
+            UpdateAccountInfo();
+            MessageBox.Show($"Now using {(useUnlockedMethods ? "Unlocked" : "Locked")} methods.");
+        }
+
+        private void UpdateAccountInfo()
+        {
+            if (currentAccount != null)
             {
-                Console.WriteLine(" your current balance is : " + activeAccount.getBalance());
-                Console.WriteLine(" (prese enter to continue)");
-                Console.ReadLine();
+                // This method needs to be executed in a way that it doesn't freeze the UI.
+                Task.Run(() =>
+                {
+                    int balance = useUnlockedMethods ? currentBankSystem.DispatchATM().GetAccountBalanceUnlocked(currentAccount)
+                                                     : currentBankSystem.DispatchATM().GetAccountBalance(currentAccount);
+                    this.Invoke(new Action(() =>
+                    {
+                        lblAccountInfo.Text = $"Balance: {balance}";
+                    }));
+                });
             }
         }
     }
-
 }
